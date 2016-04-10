5adeac14aa456f5a01743d20a9bb0a3e194116f5
Fonsvanh <alfons.vanhoudt@skynet.be>
2016-04-10 16:08:33 +0200

    Merge remote-tracking branch 'origin/master'

--- xmlrpc.php
+++ xmlrpc.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 /**
@@ -16,3 +17,23 @@ include_once DRUPAL_ROOT . '/includes/xmlrpc.inc';
 include_once DRUPAL_ROOT . '/includes/xmlrpcs.inc';
 
 xmlrpc_server(module_invoke_all('xmlrpc'));
+=======
+<?php
+
+/**
+ * @file
+ * PHP page for handling incoming XML-RPC requests from clients.
+ */
+
+/**
+ * Root directory of Drupal installation.
+ */
+define('DRUPAL_ROOT', getcwd());
+
+include_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+include_once DRUPAL_ROOT . '/includes/xmlrpc.inc';
+include_once DRUPAL_ROOT . '/includes/xmlrpcs.inc';
+
+xmlrpc_server(module_invoke_all('xmlrpc'));
+>>>>>>> origin/master
